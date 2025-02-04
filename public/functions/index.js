ff4c3138280862dac34137a3d0877b98113d33c0
Farbod Shabani <shabani.farbod@yahoo.com>
2025-02-04 23:03:23 +1100

    adding jahan resturant and removing animation

--- public/functions/index.js
+++ public/functions/index.js
@@ -3,6 +3,7 @@ const pages = document.querySelectorAll(".content-container");
 const collapseNavbar = document.getElementById("collapse-navbar");
 const gatheringPage = document.getElementById("gathering-page");
 const bluePoint = document.getElementById("bpr");
+const jahanResturant = document.getElementById("jr");
 const bpfimage = document.getElementsByClassName("first-image")[0];
 const bpsimage = document.getElementsByClassName("second-image")[0];
 const bptimage = document.getElementsByClassName("third-image")[0];
@@ -31,12 +32,36 @@ bluePoint.addEventListener("touchstart", () => {
   bpfimage?.classList?.add("first-image-hover");
   bpsimage?.classList?.add("second-image-hover");
   bptimage?.classList?.add("third-image-hover");
+  bpfoimage?.classList?.remove("first-image-hover");
+  bpfiimage?.classList?.remove("second-image-hover");
+  bpsiimage?.classList?.remove("third-image-hover");
 });
 
 bluePoint.addEventListener("touchmove", () => {
   bpfimage?.classList?.add("first-image-hover");
   bpsimage?.classList?.add("second-image-hover");
   bptimage?.classList?.add("third-image-hover");
+  bpfoimage?.classList?.remove("first-image-hover");
+  bpfiimage?.classList?.remove("second-image-hover");
+  bpsiimage?.classList?.remove("third-image-hover");
+});
+
+jahanResturant.addEventListener("touchstart", () => {
+  bpfoimage?.classList?.add("first-image-hover");
+  bpfiimage?.classList?.add("second-image-hover");
+  bpsiimage?.classList?.add("third-image-hover");
+  bpfimage?.classList?.remove("first-image-hover");
+  bpsimage?.classList?.remove("second-image-hover");
+  bptimage?.classList?.remove("third-image-hover");
+});
+
+jahanResturant.addEventListener("touchmove", () => {
+  bpfoimage?.classList?.add("first-image-hover");
+  bpfiimage?.classList?.add("second-image-hover");
+  bpsiimage?.classList?.add("third-image-hover");
+  bpfimage?.classList?.remove("first-image-hover");
+  bpsimage?.classList?.remove("second-image-hover");
+  bptimage?.classList?.remove("third-image-hover");
 });
 
 const mouseHoverIn = (left) => {
